1cc588e30f0aac115f45064c2a9e380c1ad65495
fourtimes <57703276+FourTimes@users.noreply.github.com>
2021-02-05 12:25:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CI/CD React
+          demo CI/CD React
         </a>
       </header>
     </div>
