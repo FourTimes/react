58354ebc8675142e71c3be7fa63768d32038d43c
fourtimes <57703276+FourTimes@users.noreply.github.com>
2021-02-13 09:59:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 "use strict";
+const uuid = require('uuid');
+
+
 exports.handler = function (event, context, callback) {
   var response = {
     headers: {
       "Content-Type": "text/html; charset=utf-8",
     },
     statusCode: 200,
+    message: uuid.v5
     body: "<h1>Welcome to AWS API GATEWAY!!!</h1>",
   };
   callback(null, response);
